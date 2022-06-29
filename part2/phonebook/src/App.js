a75dcf34f12004848dc85b03ff32521d7e3f0338
Ravindra GV <ravindragv@users.noreply.github.com>
2022-06-29 19:09:00 +0530

    2.8: The Phonebook Step3
    + Added number field

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,9 +2,11 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas',
+      number: '040-1234567'}
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -15,6 +17,7 @@ const App = () => {
     } else {
       const personArrayElem = {
         name: newName,
+        number: newNumber
       }
       setPersons(persons.concat(personArrayElem))
     }
@@ -22,6 +25,10 @@ const App = () => {
 
   const handleNameAddition = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNumberAddition = (event) => {
+    setNewNumber(event.target.value)
   }
 
   return (
@@ -35,11 +42,17 @@ const App = () => {
                 />
         </div>
         <div>
+          number: <input
+                  value={newNumber}
+                  onChange={handleNumberAddition}
+                />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      <div>{persons.map((person) => {return <b key={person.name}>{person.name}<br/></b>})}</div>
+      <div>{persons.map((person) => {return <b key={person.name}>{person.name} {person.number}<br/></b>})}</div>
     </div>
   )
 }

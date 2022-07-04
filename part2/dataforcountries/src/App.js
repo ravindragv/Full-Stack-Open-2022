137ba6c61221f5c5c52482bf50a65c54327650d5
Ravindra GV <ravindragv@users.noreply.github.com>
2022-07-04 23:42:51 +0530

    2.13*: Data for countries, step2
    + Add Show button for each country. When clicked on it, it will display
    the details of that particular country

--- part2/dataforcountries/src/App.js
+++ part2/dataforcountries/src/App.js
@@ -1,6 +1,5 @@
 import axios from "axios";
-import React, { useEffect } from "react";
-import {useState} from 'react'
+import React, { useState, useEffect } from "react";
 import InputField from './components/InputField'
 import Countries from "./components/Countries";
 
--- part2/dataforcountries/src/components/Countries.js
+++ part2/dataforcountries/src/components/Countries.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Country from "./Country";
 
 const Countries = ({countries}) => {
+    // We know that maximum of 10 countries will be displayed so create an
+    // array of 10 elements
+    const visibilityInitArray = new Array(10).fill(false)
+    const [visibility, setVisibility] = useState(visibilityInitArray)
+
+    const handleShow = (i) => {
+        const visibilityRef = [...visibility]
+        visibilityRef[i] = !visibilityRef[i]
+        setVisibility(visibilityRef)
+    }
+
     if (countries.length === 1) {
         return (<Country country={countries[0]}/>)
     } else if (countries.length > 10) {
@@ -10,8 +21,12 @@ const Countries = ({countries}) => {
 
     return(
         <div>
-            {countries.map((element) => 
-                <div key={element.cca3}>{element.name.common}</div>
+            {countries.map((element, i) =>
+                <div key={element.cca3}>
+                    {element.name.common}
+                    <button onClick={() => handleShow(i)}>{visibility[i]? 'Hide' : 'Show'}</button>
+                    {visibility[i] && <Country country={countries[i]}/>}
+                </div>
             )}
         </div>
     )
